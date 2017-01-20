dc22624545fa3d5eb653b9c7803286159211a233
teo.mertanen <teo.mertanen@gofore.com>
2017-01-21 00:50:07 +0200

    extract Player from PlayerInput, spawn 4 players

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -3,34 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour {
-
-	enum PlayerState { Dead, Alive }
+	
 	enum GameState { NotStarted, Started, Paused, Over }
 
-	// Player and players are placeholders 
-	class Player
-	{
-		public PlayerState state;
-
-		public bool IsAlive()
-		{
-			return this.state.Equals(PlayerState.Alive);
-		}
-	}
-	
 	GameState currentState = GameState.NotStarted;
 	List<Player> players = new List<Player>();
+
+	[SerializeField]
+	private GameObject playerPrefab;
 
 
 	// Use this for initialization
 	void Start ()
 	{
+		Debug.Log("Start game logic loop");
 		Switch(currentState, GameState.Started);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		Debug.Log ("Update game logic loop");
 		if (currentState != GameState.Paused) {
 			// Check if game should end
 			if (IsGameOverConditionReached ()) {
@@ -40,6 +33,15 @@ public class GameLogic : MonoBehaviour {
 			// Check if player should respawn
 			players.ForEach (p => RespawnPlayerIfNeeded (p));
 		}
+	}
+
+	void CreatePlayer(int playerNumber)
+	{
+		Vector3 v3 = new Vector3 (10, 10, 0 + 1*playerNumber);
+		GameObject playerObj = Instantiate(playerPrefab, v3, Quaternion.identity);
+		PlayerInput playerInput = playerObj.GetComponent<PlayerInput>();
+		Player player = playerObj.GetComponent<Player>();
+		playerInput.init(playerNumber, player);
 	}
 
 	bool IsGameOverConditionReached()
@@ -64,7 +66,11 @@ public class GameLogic : MonoBehaviour {
 		{
 		case GameState.Started:
 			// instantiate four players
-			break;
+			List<int> playercounts = new List<int> (new int[] { 1, 2, 3, 4 });
+			playercounts.ForEach (
+				i => CreatePlayer(i)
+			);
+			break;	
 		case GameState.Paused:
 			break;
 		case GameState.Over:
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	enum PlayerState { Dead, Alive }
+
+	[SerializeField]
+	private int playerNumber;
+	[SerializeField]
+	private float movementSpeed = 1f;
+	[SerializeField]
+	private PlayerState currentState = PlayerState.Alive;
+	[SerializeField]
+	private GameObject shockWavePrefab;
+
+	private Rigidbody rb;
+	private float chargeStartTime = 0f;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		gameObject.layer = LayerMask.NameToLayer("Player1");
+	}
+
+	void Update()
+	{
+		//
+	}
+
+	public bool IsAlive()
+	{
+		return (currentState == PlayerState.Alive);
+	}
+
+	public void Move(float x, float z)
+	{
+		if (this.IsAlive ())
+		{
+			Vector3 delta = new Vector3 (x, 0, z);
+
+			transform.position += delta * movementSpeed;
+		}
+	}
+
+	public void StartCharging() 
+	{
+		Debug.Log("player " + " started charging");
+		chargeStartTime = Time.time;
+	}
+
+	public void StopCharging()
+	{
+		float chargeTime = Time.time - chargeStartTime;
+		Debug.Log("player " + " charged for " + chargeTime + " seconds");
+		ReleaseShockWave(chargeTime);
+	}
+
+	void ReleaseShockWave(float time)
+	{
+		GameObject shockWaveObj = Instantiate(shockWavePrefab, transform.position, Quaternion.identity);
+		shockWaveObj.layer = gameObject.layer;
+		ShockWave shockWave = shockWaveObj.GetComponent<ShockWave>();
+		shockWave.propagationSpeed *= time;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.layer == LayerMask.NameToLayer("Death"))
+		{
+			currentState = PlayerState.Dead;
+		}
+
+		Debug.Log("player " + playerNumber + " collided with " + col.gameObject.name + " of layer " + LayerMask.LayerToName(col.gameObject.layer));
+
+		if (LayerMask.LayerToName(col.gameObject.layer).Contains("Player"))
+		{
+			col.gameObject.SetActive(false);
+			Vector3 force = transform.position - col.contacts[0].point;
+			rb.AddForce(force * 10f, ForceMode.Impulse);
+		}
+	}
+}
