d05723f0b7050d28fc5af825d1f488f03ef0e66c
Henri <henri@futurefly.net>
2017-01-20 23:14:31 +0200

    - Basic player movement and shockwave shooting

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	enum PlayerState { Dead, Alive }
+
+	[SerializeField]
+	private int playerNumber;
+	[SerializeField]
+	private float movementSpeed = 1f;
+	[SerializeField]
+	private PlayerState currentState = PlayerState.Alive;
+	[SerializeField]
+	private GameObject shockWavePrefab;
+
+	private Rigidbody rb;
+	private int button0Index = -1;
+	private float chargeStartTime = 0f;
+
+	KeyCode GetJoystickButton(int index)
+	{
+		return (KeyCode)(button0Index + index);
+	}
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		button0Index = (int)KeyCode.JoystickButton0 + playerNumber * 20;
+
+		gameObject.layer = LayerMask.NameToLayer("Player" + playerNumber);
+	}
+
+	void Update ()
+	{
+		if (currentState == PlayerState.Alive)
+		{
+			float x = Input.GetAxis("p" + playerNumber.ToString().ToLower() + " left x");
+			float z = Input.GetAxis("p" + playerNumber.ToString().ToLower() + " left y");
+
+			Vector3 delta = new Vector3(x, 0, z);
+
+			transform.position += delta * movementSpeed;
+
+			if (Input.GetKeyDown(GetJoystickButton(1)))
+			{
+				// Debug.Log("player " + playerNumber + " pressed button " + GetJoystickButton(1).ToString());
+				Debug.Log("player " + playerNumber + " started charging");
+				chargeStartTime = Time.time;
+			}
+
+			if (Input.GetKeyUp(GetJoystickButton(1)))
+			{
+				// Debug.Log("player " + playerNumber + " pressed button " + GetJoystickButton(1).ToString());
+				float chargeTime = Time.time - chargeStartTime;
+				Debug.Log("player " + playerNumber + " charged for " + chargeTime + " seconds");
+				ReleaseShockWave(chargeTime);
+			}
+		}
+	}
+
+	void ReleaseShockWave(float time)
+	{
+		GameObject shockWaveObj = Instantiate(shockWavePrefab, transform.position, Quaternion.identity);
+		shockWaveObj.layer = gameObject.layer;
+		ShockWave shockWave = shockWaveObj.GetComponent<ShockWave>();
+		shockWave.propagationSpeed *= time;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.layer == LayerMask.NameToLayer("Death"))
+		{
+			currentState = PlayerState.Dead;
+		}
+
+		Debug.Log("player " + playerNumber + " collided with " + col.gameObject.name + " of layer " + LayerMask.LayerToName(col.gameObject.layer));
+
+		if (LayerMask.LayerToName(col.gameObject.layer).Contains("Player"))
+		{
+			col.gameObject.SetActive(false);
+			Vector3 force = transform.position - col.contacts[0].point;
+			rb.AddForce(force * 10f, ForceMode.Impulse);
+		}
+	}
+}
