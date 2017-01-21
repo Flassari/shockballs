03706d14e333f481eb939155249c67830a2bb22a
Ari Arnbjörnsson <154301+Flassari@users.noreply.github.com>
2017-01-21 15:59:36 +0200

    Add quick sound library

---
+++ Assets/Scripts/Audio/CompositeSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Composite Sound", fileName = "Composite Sound")]
+public class CompositeSound : ScriptableObject, ISound
+{
+	public SoundData[] sounds;
+
+	public void Play(Vector3 position, float volume)
+	{
+		foreach (SoundData sound in sounds) {
+			sound.Play(position, volume);
+		}
+	}
+}
---
+++ Assets/Scripts/Audio/ISound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public interface ISound
+{
+	void Play(Vector3 position, float volume);
+}
+
+[Serializable]
+public class SoundData
+{
+	public UnityEngine.Object sound;
+	public float volume = 1.0f;
+
+	public virtual void Play(Vector3 position, float volume)
+	{
+		if (sound is AudioClip) {
+			AudioSource.PlayClipAtPoint((AudioClip)sound, position, volume * this.volume);
+		}
+		else if (sound is ISound) {
+			((ISound)sound).Play(position, volume);
+		}
+		else {
+			throw new UnityException("Sound " + sound.name + " type " + sound.GetType() + " unsupported.");
+		}
+	}
+}
