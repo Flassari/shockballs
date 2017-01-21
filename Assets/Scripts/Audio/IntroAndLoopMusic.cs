a7a13424ccb2ca3032a9ec7611b290c6b8583adf
Ari Arnbjörnsson <ari@flassari.is>
2017-01-21 21:14:55 +0200

    Add IntroAndLoopMusic component

---
+++ Assets/Scripts/Audio/IntroAndLoopMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAndLoopMusic : MonoBehaviour
+{
+	public AudioSource intro;
+	public AudioSource loop;
+
+	void Start ()
+	{
+		intro.Play();
+		loop.PlayDelayed(intro.clip.length);
+	}
+}
