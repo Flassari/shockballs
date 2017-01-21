2a7ce3ee3c4d9e741ef020648fb3d70a94478fde
Ari Arnbjörnsson <154301+Flassari@users.noreply.github.com>
2017-01-21 22:25:57 +0200

    Try to fix collider anomalies

---
+++ Assets/Scripts/ColliderDelayedActivate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDelayedActivate : MonoBehaviour
+{
+	public Collider[] colliders;
+
+	protected void Awake()
+	{
+		SetCollidersEnabled(false);
+	}
+
+	protected IEnumerator Start()
+	{
+		yield return null;
+		SetCollidersEnabled(true);
+	}
+
+	#if UNITY_EDITOR
+	protected void OnValidate()
+	{
+		SetCollidersEnabled(false);
+	}
+	#endif
+
+	private void SetCollidersEnabled(bool enabled)
+	{
+		foreach (Collider collider in colliders)
+		{
+			if (collider == null) continue;
+
+			collider.enabled = enabled;
+		}
+	}
+}
