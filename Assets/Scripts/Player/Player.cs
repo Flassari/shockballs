2f2e9ff66eaba4296efede2d1b3aa985f94c020e
teo.mertanen <teo.mertanen@gofore.com>
2017-01-21 14:59:30 +0200

    stop ball when respawning

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -78,6 +78,7 @@ public class Player : MonoBehaviour
 		currentState = PlayerState.Alive;
 		transform.position = position;
 		mass = originalMass;
+		rb.velocity = Vector3.zero;
 		Scale(mass);
 	}
 
