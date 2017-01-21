5d9c8f7b7df988d7663d8e2b4cdbdfc7a6ba53bf
Ari Arnbjörnsson <ari@flassari.is>
2017-01-21 15:59:36 +0200

    Add quick sound library

---
+++ Assets/Scripts/Audio/RandomSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Random Sound", fileName = "Random Sound")]
+public class RandomSound : ScriptableObject, ISound
+{
+	public SoundData[] sounds;
+
+	public void Play(Vector3 position, float volume)
+	{
+		if (sounds.Length == 0) return;
+
+		sounds[Random.Range(0, sounds.Length)].Play(position, volume);
+	}
+}
