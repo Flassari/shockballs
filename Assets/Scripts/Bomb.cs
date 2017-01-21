f5997a5f9aa87755544ae137ccdae0163ae1bf43
Ari Arnbjörnsson <154301+Flassari@users.noreply.github.com>
2017-01-21 16:56:15 +0200

    Add bombs

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+	public ShockWave shockwavePrefab;
+	
+	[HideInInspector] public float fuseTimeout;
+	[HideInInspector] public float shockwavePower;
+	[HideInInspector] public Color shockwaveColor;
+
+	private float spawnTime;
+
+	protected void Start()
+	{
+		spawnTime = Time.time;
+	}
+
+	protected void Update()
+	{
+		if (Time.time > spawnTime + fuseTimeout)
+		{
+			ShockWave shockWave = Instantiate(shockwavePrefab);
+			shockWave.transform.position = transform.position;
+			shockWave.color = shockwaveColor;
+			shockWave.power = shockwavePower;
+			Destroy(gameObject);
+		}
+	}
+}
