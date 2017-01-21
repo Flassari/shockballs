a76a1fe5198cb949f589b818896264a2c3ab63b4
Ari Arnbjörnsson <154301+Flassari@users.noreply.github.com>
2017-01-22 01:21:48 +0200

    Add bomb explode sound

--- Assets/Scripts/Audio/ISound.cs
+++ Assets/Scripts/Audio/ISound.cs
@@ -14,7 +14,7 @@ public class SoundData
 	public UnityEngine.Object sound;
 	public float volume = 1.0f;
 
-	public virtual void Play(Vector3 position, float volume)
+	public virtual void Play(Vector3 position, float volume = 1)
 	{
 		if (sound is AudioClip) {
 			AudioSource.PlayClipAtPoint((AudioClip)sound, position, volume * this.volume);
