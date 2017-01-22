05e5f3d7c49440220ed24e8f503d9f8b9ec7e79a
Ari Arnbjörnsson <154301+Flassari@users.noreply.github.com>
2017-01-22 13:24:24 +0200

    Add water overlay

---
+++ Assets/Scripts/WaterOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterOverlay : MonoBehaviour
+{
+	public float speed = 1;
+	private Material material;
+
+	void Start () {
+		material = GetComponent<Renderer>().material;
+		
+	}
+
+	void Update () {
+		material.SetTextureOffset("_MainTex", new Vector2(Time.time * speed, 0));
+	}
+}
